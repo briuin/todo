a246634511359db27c63bd57ef7efc0d99da6bb9
briuin <dreamingdexiaoxiaohao@hotmail.com>
2024-08-15 00:35:59 +0800

    initial test project

--- server/todo/Controllers/WeatherForecastController.cs
+++ server/todo/Controllers/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace server.Controllers;
+namespace todo.Controllers;
 
 [ApiController]
 [Route("[controller]")]
--- server/todo/WeatherForecast.cs
+++ server/todo/WeatherForecast.cs
@@ -1,4 +1,4 @@
-namespace server;
+namespace todo;
 
 public class WeatherForecast
 {
